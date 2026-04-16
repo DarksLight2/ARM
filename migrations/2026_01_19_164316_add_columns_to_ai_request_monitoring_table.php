2094e2faf3c348af3efed42b7a7595ee5bd48eff
Maksym Kovalets <maksym.kovalets@gmail.com>
2026-04-16 20:45:27 +0200

    meta column changed default value

--- migrations/2026_01_19_164316_add_columns_to_ai_request_monitoring_table.php
+++ migrations/2026_01_19_164316_add_columns_to_ai_request_monitoring_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,7 +11,7 @@ return new class extends Migration
     {
         Schema::table('ai_request_monitoring', function (Blueprint $table) {
             $table->string('status')->nullable();
-            $table->json('meta')->default('[]');
+            $table->json('meta')->default(new Expression('(JSON_ARRAY())'));
         });
     }
 
